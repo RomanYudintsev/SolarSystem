ffb8088f9c2a60a004fd46b303eb9827e1d764b0
Юдинцев Роман Васильевич <r.yudintsev@2gis.ru>
2017-05-06 01:54:24 +0700

    custom texture, and test text on texture @2

--- src/SpaceBodyParams.hx
+++ src/SpaceBodyParams.hx
@@ -1,4 +1,9 @@
 package ;
+import openfl.text.TextFormat;
+import openfl.text.TextFieldAutoSize;
+import openfl.text.TextField;
+import openfl.display.Sprite;
+import openfl.display.BitmapData;
 import away3d.materials.lightpickers.StaticLightPicker;
 import away3d.materials.TextureMaterial;
 import openfl.Assets;
@@ -52,12 +57,50 @@ class SpaceBodyParams {
     {
         var skinMaterial:MaterialBase = null;
         try {
-            skinMaterial = new TextureMaterial(new BitmapTexture(Assets.getBitmapData("assets/"+srcValue)));
+            skinMaterial = new TextureMaterial(generateTexture(srcValue));
         } catch (ex : Dynamic) {}
         try {
             skinMaterial = new ColorMaterial(cast (srcValue, Int));
         } catch (ex : Dynamic) {}
         return skinMaterial;
+    }
+
+    private function generateTexture(assetName:String = null):BitmapTexture
+    {
+        var bitmapData = assetName != null ? Assets.getBitmapData("assets/"+assetName) : generateBitmapData();
+        return new BitmapTexture(bitmapData);
+    }
+
+    private function generateBitmapData():BitmapData
+    {
+        var w = 64;
+        var h = Math.round(w/2);
+        var tmpl:Sprite = new Sprite();
+
+        var label = new TextField();
+        var tft = new TextFormat();
+        tft.size = 10;
+        tft.color = 0x0000FF;
+        label.text = 'test';
+        label.setTextFormat(tft, 0, label.length);
+        label.autoSize = TextFieldAutoSize.LEFT;
+        label.x = w/2 - label.width/2;
+        label.y = h/2 - label.height/2;
+
+        tmpl.graphics.beginFill(0xFFFFFF, 1);
+        tmpl.graphics.drawRect(0,0, w, h);
+        tmpl.graphics.endFill();
+
+        tmpl.graphics.beginFill(0x00FF00, 1);
+        tmpl.graphics.drawCircle(w/2,h/2, h/4);
+        tmpl.graphics.endFill();
+
+
+        tmpl.addChild(label);
+
+        var bd = new BitmapData(w, h, true, 0x0);
+        bd.draw(tmpl);
+        return bd;
     }
 
     public static function newFromYaml(infoYaml:AnyObjectMap):SpaceBodyParams
